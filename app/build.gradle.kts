26d29f2864f2943c949ef78faf928e249ab326bd
e4riya <kotkovdima0@gmail.com>
2026-07-06 15:24:49 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
     id("org.sonarqube") version "6.3.1.5724"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
-application{
+
+application {
     mainClass = "hexlet.code.App"
 }
 
@@ -19,27 +20,38 @@ repositories {
 
 dependencies {
     implementation("io.javalin:javalin:6.1.3")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("io.javalin:javalin-bundle:6.6.0")
-    implementation("com.h2database:h2:2.2.220")
+
+    implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("com.zaxxer:HikariCP:5.0.1")
+
+    implementation("com.h2database:h2:2.2.220")
+    runtimeOnly("org.postgresql:postgresql:42.7.7")
+
     implementation("gg.jte:jte:3.1.9")
     implementation("com.konghq:unirest-java:3.14.5")
+    implementation("org.jsoup:jsoup:1.21.2")
+
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("org.jsoup:jsoup:1.21.2")
 
     testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
 }
-tasks.jacocoTestReport {reports{ xml.required.set(true) } }
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
 
 tasks.test {
     useJUnitPlatform()
 }
+
 sonar {
     properties {
         property("sonar.projectKey", "e4riya_java-project-72")
